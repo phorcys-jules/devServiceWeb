2e055377f9aff8a48e2f8ef4ca50c4182ee5df5d
Jules <jfrancois968@gmail.com>
2022-03-01 17:33:52 +0100

    ✨ TD4

--- tds/Readme.md
+++ tds/Readme.md
@@ -1,13 +1,10 @@
 install with npm i
 run with npm start
 
-routes : 
-● lister les annonces,  
-● lister les annonces du 54,
-● lister les annonces de la catégorie 'voitures',
-● créer une catégorie,
-● modifier une annonce existante,
-● créer une annonce, l'associer à une catégorie et un département,
-● modifier la catégorie d'une annonce,
-● ajouter une image à une annonce,
-● supprimer une annonce et toutes les images associées.
+Routes : 
+    ✔️Lister les commandes : http://localhost:8321/commandes
+    ✔️Obtenir les infos sur 1 commande : http://localhost:8321/commandes/id
+    ✔️Obtenir les infos détaillé sur 1 commande : http://localhost:8321/commandes/id?embed=items
+    ✔️Modifier une commande : PUT http://localhost:8321/commandes/id
+        body : {nom, mail, livraison}
+    ✔️Obtenir la liste des items d'une commande http://localhost:8321/commandes/id/items
--- tds/service/api/routes/commandes.js
+++ tds/service/api/routes/commandes.js
@@ -24,13 +24,89 @@ router.get('/', async function (req, res, next) {
 });
 
 router.get('/:id', async function (req, res, next) {
-  let commande;
+  let commande ={type : "ressource"};
+  let toSend;
   try {
-    commande = await database.select().from('commande').where('id', req.params.id);
+    commande.commande = (await database.select('id', 'mail', 'nom', 'created_at', 'livraison', 'montant').from('commande').where('id', req.params.id))[0];
+
   } catch (error) {
     res.status(500).json(erreur(500));
   }
-  console.log(commande);
+
+  //console.log(commande);
+  if (commande.data && commande.data.length === 0) {
+    res.status(404).json({
+      "type": "error",
+      "error": 404,
+      "message": `commande ${req.params.id} non trouvée`
+    });
+  } else {
+    //mode détaillé ou l'on ajoute les items : 
+    
+    if (req.query.embed && req.query.embed === "items") {
+      try {
+        items = await database.select('id', 'uri', 'libelle', 'tarif', 'quantite').from('item').where('command_id', req.params.id)
+      } catch (error) {
+        //res.status(500).send(error);
+        res.status(500).json(erreur(500));
+      }
+      commande.items = items;
+    }
+    commande.links = {
+      "items": {
+        "href": `/commandes/${req.params.id}/items`
+      },
+      "self": {
+        "href": `/commandes/${req.params.id}`
+      },
+    }
+    //toSend  commande;
+    //console.log("commande détaillé + lien : ", commande);
+    //console.log("toSend : ", toSend);
+
+    res.status(200).json(commande);
+
+  }
+});
+
+//: nom du client, mail du client, date et heure de livraison.
+router.put('/:id', async function (req, res, next) {
+  let commande;
+  try {
+    commande = await database.from('commande').where('id', req.params.id).update({
+      nom: req.body.nom,
+      mail: req.body.mail,
+      livraison: req.body.livraison,
+      updated_at: new Date()
+    });
+  } catch (error) {
+    //console.log(error);
+    res.status(500).json(erreur(500));
+  }
+  //affected rows = 0
+  if (commande === 0) {
+    res.status(404).json({
+      "type": "error",
+      "error": 404,
+      "message": `commande ${req.params.id} non trouvée`
+    });
+  } else {
+    res.status(204).json(commande);
+  }
+});
+
+router.get('/:id/items', async function (req, res, next) {
+  let items;
+  let commande;
+  try {
+    //.join('item', 'item.command_id', 'commande.id');
+    commande = await database.select().from('commande').where('commande.id', req.params.id);
+    items = await database.select('id', 'uri', 'libelle', 'tarif', 'quantite').from('item').where('command_id', req.params.id)
+  } catch (error) {
+    //res.status(500).send(error);
+    res.status(500).json(erreur(500));
+  }
+
   if (commande && commande.length === 0) {
     res.status(404).json({
       "type": "error",
@@ -38,12 +114,19 @@ router.get('/:id', async function (req, res, next) {
       "message": `commande ${req.params.id} non trouvée`
     });
   } else {
+    commande = {
+      "type": "collection",
+      "count": items.length,
+      items
+    }
+
     res.json(commande);
   }
 });
 
+//TODO
 //: nom du client, mail du client, date et heure de livraison.
-router.put('/:id', async function (req, res, next) {
+router.post('/:id', async function (req, res, next) {
   let commande;
   try {
     commande = await database.from('commande').where('id', req.params.id).update({
@@ -75,7 +158,7 @@ function erreur(type) {
       return {
         "type": "error",
         "error": 500,
-        "message": `impossible d'éxècuter la requete`
+        "message": `impossible d'éxècuter la requete, vérifier la connexion à la base de données`
       };
       break;
 
